31b61947b90365fd3f59c23268b4a122c3a681c1
deathmond1987 <deathmond1987@gmail.com>
2024-06-06 11:09:09 +0300

    Update git_cron.sh

--- git_cron.sh
+++ git_cron.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 set -xe
-USER=deathmond1987
-PROJECT_LIST=$(curl https://api.github.com/users/$USER/repos\?page\=1\&per_page\=100 | grep -e 'clone_url' | cut -d \" -f 4 | sed '/WSA/d' | xargs -L1)
-GIT_DIR=~
+while [ "$1" != "" ]; do
+   case "$1" in
+       -u|--user) shift
+                  GH_USER=$1
+                  ;;
+       *) echo "unknown arg: $1"
+          exit 1 >2
+                  ;;
+    esac
+    shift
+done
 
-cd "${GIT_DIR}"
+GH_USER=${GH_USER:=deathmond1987}
+PROJECT_LIST=$(curl https://api.github.com/users/$GH_USER/repos\?page\=1\&per_page\=100 | grep -e 'clone_url' | cut -d \" -f 4 | sed '/WSA/d' | xargs -L1)
+#GIT_DIR=~
+
+#cd "${GIT_DIR}"
 for project in ${PROJECT_LIST}; do
     project_name=$(echo "${project}" | cut -d'/' -f 5)
     if [ -d ./"${project_name//.git/}" ]; then
